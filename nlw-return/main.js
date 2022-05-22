b5c904bc63ee3c1d9c4aa97fd9e3962cee4c1055
Kevin Benevides <61016019+kevinbsr@users.noreply.github.com>
2022-05-21 21:22:59 -0300

    Update

--- nlw-return/main.js
+++ nlw-return/main.js
@@ -1,8 +1,25 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
 function onScroll() {
+  showNavOnScroll()  
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 500) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
 }
 
@@ -13,6 +30,7 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
 
 ScrollReveal({
   origin: 'top',
@@ -27,4 +45,4 @@ ScrollReveal({
 #services .card,
 #about,
 #about header,
-#about .content`);
+#about .content`)
