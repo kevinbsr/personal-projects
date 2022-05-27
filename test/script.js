4a93f0fe743647276785b63ed0c7cf35a94a1551
Kevin Benevides <61016019+kevinbsr@users.noreply.github.com>
2022-05-27 17:32:49 -0300

    Update

---
+++ test/script.js
@@ -0,0 +1,11 @@
+function changeToGreen() {
+    addEventListener('click', function() {
+        document.getElementById('title').style.color = 'green';
+    });
+}
+
+function changeToBlue() {
+    addEventListener('click', function() {
+        document.getElementById('title').style.color = 'blue';
+    });
+}
